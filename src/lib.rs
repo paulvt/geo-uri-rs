537d94485880ddc2df430dae4c4887c6f797c456
Paul van Tilburg <paul@luon.net>
2022-09-27 18:01:26 +0200

    Set some warnings/error lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 #![doc = include_str!("../README.md")]
+#![warn(
+    clippy::all,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
+    trivial_numeric_casts
+)]
+#![deny(missing_docs)]
 
 use std::fmt;
 use std::num::{ParseFloatError, ParseIntError};
